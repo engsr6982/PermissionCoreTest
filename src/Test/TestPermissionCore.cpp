f7d5ea1a91d0ccb7a2c435c0ab9846df697b8fb3
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-03-27 21:52:55 +0800

    chore: sync

--- src/Test/TestPermissionCore.cpp
+++ src/Test/TestPermissionCore.cpp
@@ -1,5 +1,4 @@
 #include "Global.h"
-#include <Entry/Macros.h>
 #include <PermissionCore/PermissionCore.h>
 #include <PermissionCore/PermissionManager.h>
 #include <cassert>
@@ -28,7 +27,7 @@ void testManagerGetPermissionCore() {
     auto permCore = manager.getPermissionCore("test");
 
     // permCore is nullptr if the permission core is not registered
-    std::cout << "getPermissionCore test value: " << (permCore == nullptr) << std::endl;
+    std::cout << "getPermissionCore test value: " << (permCore == nullptr ? "Fail" : "Success") << std::endl;
 }
 
 void testPermissionCore() {
@@ -39,49 +38,66 @@ void testPermissionCore() {
     auto core = manager.getPermissionCore("test");
 
     if (core != nullptr) {
-        // Test admin functions
+        // 测试添加删除管理员
         core->addAdmin("admin1");
         core->addAdmin("admin2");
         assert(core->isAdmin("admin1") == true);
         assert(core->isAdmin("admin2") == true);
         assert(core->isAdmin("admin3") == false);
         core->removeAdmin("admin1");
+        core->removeAdmin("admin2");
         assert(core->isAdmin("admin1") == false);
+        assert(core->isAdmin("admin2") == false);
 
-        // Test user group functions
+        // 测试创建删除组
         core->createUserGroup("group1");
         assert(core->hasUserGroup("group1") == true);
         assert(core->hasUserGroup("group2") == false);
         core->deleteUserGroup("group1");
         assert(core->hasUserGroup("group1") == false);
 
-        // Test user functions
+        // 测试重命名
+        core->createUserGroup("group2");
+        assert(core->hasUserGroup("group2") == true);
+        core->renameUserGroup("group2", "group1");
+        assert(core->hasUserGroup("group1") == true);
+        assert(core->hasUserGroup("group2") == false);
+
+        // 测试用户添加删除
         core->addUserToUserGroup("group1", "user1");
         core->addUserToUserGroup("group1", "user2");
         assert(core->isUserInUserGroup("group1", "user1") == true);
         assert(core->isUserInUserGroup("group1", "user3") == false);
         core->removeUserToUserGroup("group1", "user1");
+        core->removeUserToUserGroup("group1", "user2");
         assert(core->isUserInUserGroup("group1", "user1") == false);
+        assert(core->isUserInUserGroup("group1", "user2") == false);
 
-        // Test permission functions
+        // 测试权限添加删除
         core->addPermissionToUserGroup("group1", "permission1");
         core->addPermissionToUserGroup("group1", "permission2");
         assert(core->hasUserGroupPermission("group1", "permission1") == true);
         assert(core->hasUserGroupPermission("group1", "permission3") == false);
         core->removePermissionToUserGroup("group1", "permission1");
+        core->removePermissionToUserGroup("group1", "permission2");
         assert(core->hasUserGroupPermission("group1", "permission1") == false);
+        assert(core->hasUserGroupPermission("group1", "permission2") == false);
 
-        // Test public group functions
+        // 测试公共组
         core->addPermissionToPublicGroup("permission1");
         core->addPermissionToPublicGroup("permission2");
         assert(core->hasPublicGroupPermission("permission1") == true);
         assert(core->hasPublicGroupPermission("permission3") == false);
         core->removePermissionToPublicGroup("permission1");
+        core->removePermissionToPublicGroup("permission2");
         assert(core->hasPublicGroupPermission("permission1") == false);
+        assert(core->hasPublicGroupPermission("permission2") == false);
 
-        // Test checkUserPermission
-        assert(core->checkUserPermission("user1", "permission1", true, false) == true);
+        // 测试检查权限函数
         assert(core->checkUserPermission("user1", "permission1", false, false) == false);
+        core->addPermissionToPublicGroup("permission1");
+        assert(core->checkUserPermission("user1", "permission1", true, false) == true);
+        core->addAdmin("admin1");
         assert(core->checkUserPermission("admin1", "permission1", false, true) == true);
         assert(core->checkUserPermission("admin1", "permission1", true, true) == true);
     }
--- src/Test/TestRegisterPerm.cpp
+++ src/Test/TestRegisterPerm.cpp
@@ -1,8 +1,7 @@
 #include "Global.h"
-#include <Entry/Macros.h>
-#include <Registers/Registers.h>
+#include <PermissionCore/Registers.h>
 #include <cassert>
-
+#include <entry/Entry.h>
 
 namespace test {
 
@@ -11,8 +10,8 @@ void testRegisterPermission() {
     // registerperm
     {
         bool result1 = perm::registers::registerPermission("plugin1", "permission1", "value1");
-        bool result2 = perm::registers::registerPermission("plugin2", "permission2", "value2");
-        bool result3 = perm::registers::registerPermission("plugin3", "permission3", "value3");
+        bool result2 = perm::registers::registerPermission("plugin1", "permission2", "value2");
+        bool result3 = perm::registers::registerPermission("plugin1", "permission3", "value3");
         assert(result1 == true);
         assert(result2 == true);
         assert(result3 == true);
@@ -21,32 +20,11 @@ void testRegisterPermission() {
     // test isReg
     {
         bool result1 = perm::registers::isPermissionRegistration("plugin1", "value1");
-        bool result2 = perm::registers::isPermissionRegistration("plugin2", "value2");
-        bool result3 = perm::registers::isPermissionRegistration("plugin3", "value3");
+        bool result2 = perm::registers::isPermissionRegistration("plugin1", "value2");
+        bool result3 = perm::registers::isPermissionRegistration("plugin1", "value3");
         assert(result1 == true);
         assert(result2 == true);
         assert(result3 == true);
-    }
-
-    // test getPerm
-    {
-        const std::optional<perm::registers::Authority>& result1 =
-            perm::registers::getPluginPermission("plugin1", "value1");
-        assert(result1.has_value());
-        assert(result1->permissionName == "permission1");
-        assert(result1->permissionValue == "value1");
-
-        const std::optional<perm::registers::Authority>& result2 =
-            perm::registers::getPluginPermission("plugin2", "value2");
-        assert(result2.has_value());
-        assert(result2->permissionName == "permission2");
-        assert(result2->permissionValue == "value2");
-
-        const std::optional<perm::registers::Authority>& result3 =
-            perm::registers::getPluginPermission("plugin3", "value3");
-        assert(result3.has_value());
-        assert(result3->permissionName == "permission3");
-        assert(result3->permissionValue == "value3");
     }
 
     // test getAllPerm
@@ -54,6 +32,33 @@ void testRegisterPermission() {
         const std::vector<perm::registers::Authority>& result = perm::registers::getPluginAllPermissions("plugin1");
         assert(result.size() == 3);
     }
+
+    // test getPerm
+    {
+        const std::optional<perm::registers::Authority>& result1 =
+            perm::registers::getPluginPermission("plugin1", "value1");
+        assert(result1.has_value());
+        entry::entry::getInstance().getSelf().getLogger().info(
+            "PermissionName: {}  |  PermissionValue: {}",
+            result1->permissionName,
+            result1->permissionValue
+        );
+        assert(result1->permissionName == "permission1");
+        assert(result1->permissionValue == "value1");
+
+        const std::optional<perm::registers::Authority>& result2 =
+            perm::registers::getPluginPermission("plugin1", "value2");
+        assert(result2.has_value());
+        assert(result2->permissionName == "permission2");
+        assert(result2->permissionValue == "value2");
+
+        const std::optional<perm::registers::Authority>& result3 =
+            perm::registers::getPluginPermission("plugin1", "value3");
+        assert(result3.has_value());
+        assert(result3->permissionName == "permission3");
+        assert(result3->permissionValue == "value3");
+    }
+
 
     // test unregisterPerm
     {
